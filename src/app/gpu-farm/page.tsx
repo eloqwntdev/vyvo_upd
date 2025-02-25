4d3281ddfae578362855f25ebd101c38664ae7b8
marianlukavyi <100838435+marianlukavyi@users.noreply.github.com>
2025-02-25 14:29:36 +0200

    make layout to be client

--- src/app/gpu-farm/page.tsx
+++ src/app/gpu-farm/page.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import GPUBanner from "./components/banner";
-import VyvoGPUFarms from "./components/vyvo-gpu-farms";
-import WhyBuiltFarms from "./components/why-built-farms";
-import FarmsAsAProduct from "./components/farms-as-a-product";
+// import GPUBanner from "./components/banner";
+// import VyvoGPUFarms from "./components/vyvo-gpu-farms";
+// import WhyBuiltFarms from "./components/why-built-farms";
+// import FarmsAsAProduct from "./components/farms-as-a-product";
 
 const GPUFarmPage = () => {
   return (
     <>
-      <GPUBanner />
+      {/* <GPUBanner />
       <VyvoGPUFarms />
       <WhyBuiltFarms />
-      <FarmsAsAProduct />
+      <FarmsAsAProduct /> */}
     </>
   );
 };
