065c10a905ade53ac5102b010482861b7c65132f
Emiro <emirodeve@gmail.com>
2025-04-11 04:21:41 +0330

    fix: uncomment useLottie import and View component for functionality

--- src/app/vyvo-smart-chain/components/banner/index.tsx
+++ src/app/vyvo-smart-chain/components/banner/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 import PinkButton from "@/components/common/controllers/button/pink-button";
 import React from "react";
-// import { useLottie } from "lottie-react";
+import { useLottie } from "lottie-react";
 import animationData from "../../../../../public/lottie/1/1.json";
 
 const VyvoCmartChainBanner = () => {
@@ -11,13 +11,13 @@ const VyvoCmartChainBanner = () => {
     assetsPath: "/lottie/1/images/",
   };
 
-  // const { View } = useLottie(options);
+  const { View } = useLottie(options);
 
   return (
     <section className="min-h-[578px] md:min-h-[900px] w-full flex items-center justify-center bg-black">
       <div className="w-full flex flex-col gap-20 md:gap-[160px] items-center pb-[31px] md:pb-20 px-4 md:px-6">
         <div className="flex flex-col items-center justify-center gap-6">
-          {/* {View} */}
+          {View}
           <div className="max-w-[684px] w-full flex flex-col gap-4 items-center justify-center text-center">
             <h1 className="text-white font-light font-nb text-[24px] md:text-[72px] leading-[28px] md:leading-[76px] tracking-[-0.7px] md:tracking-[-2.2px]">
               Vyvo Smart Chain
