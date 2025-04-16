2bed6b692a4ba28a00d888bd615b77d54b00ab37
Emiro <emirodeve@gmail.com>
2025-04-17 02:23:05 +0330

    fix: adjust layout and styling in AboutVSC component for improved responsiveness

--- src/app/vyvo-smart-chain/components/about-vsc/index.tsx
+++ src/app/vyvo-smart-chain/components/about-vsc/index.tsx
@@ -5,12 +5,11 @@ import secondAnimationData from "../../../../../public/lottie/3/data.json";
 import { motion, useInView, useSpring, useTransform } from "framer-motion";
 import { useRef, useEffect } from "react";
 import AnimatedCounter from "@/components/common/animations/animated-counter";
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 
 const AboutVSC = () => {
-
-  const Lottie = dynamic(() => import('lottie-react'), { 
-    ssr: false
+  const Lottie = dynamic(() => import("lottie-react"), {
+    ssr: false,
   });
 
   const sectionRef = useRef(null);
@@ -33,7 +32,7 @@ const AboutVSC = () => {
   return (
     <section
       ref={sectionRef}
-      className="py-8 md:py-12 lg:py-20 flex flex-col gap-4 md:gap-12 lg:gap-20 items-center justify-center bg-black"
+      className="py-8 md:py-12 lg:py-20 flex flex-col gap-4 md:gap-12 lg:gap-10 items-center justify-center bg-black"
     >
       <motion.span
         initial={{ opacity: 0, y: 20 }}
@@ -54,7 +53,7 @@ const AboutVSC = () => {
           to provide users with access to rewards from the data they generate
           through wearable device usage.
         </motion.p>
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-5">
+        <div className="w-[70%] grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-5">
           <div className="relative w-full">
             {/* {View} */}
             <Lottie animationData={animationData} loop />
@@ -75,7 +74,11 @@ const AboutVSC = () => {
           </div>
           <div className="relative w-full">
             {/* {View2} */}
-            <Lottie animationData={secondAnimationData} loop assetsPath="/lottie/3/images/" />
+            <Lottie
+              animationData={secondAnimationData}
+              loop
+              assetsPath="/lottie/3/images/"
+            />
             <div className="flex flex-col gap-6 md:gap-8 lg:gap-[56px] absolute bottom-7 md:bottom-8 lg:bottom-12 left-7 md:left-8 lg:left-12">
               <motion.span
                 initial={{ opacity: 0, y: 20 }}
