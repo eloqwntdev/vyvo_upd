c601737874a1bda3d2f8e6a33331963c4a7e95c7
Emiro <emirodeve@gmail.com>
2025-04-17 02:28:23 +0330

    fix: update styling for PinkButton component with new background and shadow effects

--- src/components/common/controllers/button/pink-button.tsx
+++ src/components/common/controllers/button/pink-button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 const PinkButton = ({ label, className }: ButtonProps) => {
   return (
     <button
-      className={` text-white md:text-[16px] text-[14px] leading-[18px] md:leading-[24px] font-nb py-2.5 px-[31px] bg-[#FF006699] rounded-2xl max-w-[245px] flex items-center justify-center   hover:shadow-[0px_0px_15px_5px_rgba(255,0,102,0.2)] transition-all duration-300 ease-in-out ${
+      className={` text-white md:text-[16px] text-[14px] leading-[18px] md:leading-[24px] font-nb py-2.5 px-[31px] bg-[#E749F029] max-w-[245px] flex items-center justify-center  rounded-xl bg-purple-500/16 shadow-[0px_-1px_1px_0px_rgba(231, 73, 240,0.40)_inset,_0px_1px_1px_0px_rgba(231,73,240,0.40)_inset] backdrop-blur-lg hover:shadow-[0px_0px_15px_5px_rgba(231, 73, 240,0.2)] transition-all duration-300 ease-in-out ${
         className || ""
       }`}
     >
