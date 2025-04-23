c996b5ae9d70cc82201ac554604e6b81638b3f37
Emiro Deve <emirodeve@gmail.com>
2025-04-23 16:07:54 +0330

    Implement code changes to enhance functionality and improve performance

--- src/app/vyvo-smart-chain/components/banner/index.tsx
+++ src/app/vyvo-smart-chain/components/banner/index.tsx
@@ -3,7 +3,7 @@ import { PurpleButton } from "@/components/common/controllers/button/pink-button
 import React from "react";
 // import { useLottie } from "lottie-react";
 import animationData from "../../../../../public/lottie/1/1.json";
-import animationBgPink from "../../../../../public/lottie/pink-bg/data.json";
+import animationBgPink from "../../../../../public/lottie/purple-back-lines-move/data.json";
 import dynamic from "next/dynamic";
 import { motion } from "framer-motion";
 
@@ -29,7 +29,11 @@ const VyvoCmartChainBanner = () => {
   return (
     <section className="min-h-[578px] md:min-h-[900px] w-full flex items-center justify-center bg-black relative overflow-hidden">
       <div className="absolute sm:top-[-20%] w-full h-full scale-125">
-        <Lottie animationData={animationBgPink} loop />
+        <Lottie
+          className="w-full h-full"
+          animationData={animationBgPink}
+          loop
+        />
       </div>
       <div className="w-full flex flex-col relative gap-20 md:gap-[160px] items-center pb-[31px] md:pb-20 px-4 md:px-6">
         <div className="flex flex-col items-center justify-center gap-6">
