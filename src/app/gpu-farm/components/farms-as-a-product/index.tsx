ccaada6cc10abea092cf92f6bcaf854383a5fd61
Emiro Deve <emirodeve@gmail.com>
2025-04-25 16:34:51 +0330

    Refactor code structure for improved readability and maintainability

--- src/app/gpu-farm/components/farms-as-a-product/index.tsx
+++ src/app/gpu-farm/components/farms-as-a-product/index.tsx
@@ -1,25 +1,35 @@
 "use client";
 // import { useLottie } from "lottie-react";
-import animationData from "../../../../../public/lottie/12/data.json";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import dynamic from "next/dynamic";
+import animationData from "../../../../../public/lottie/blue-back-lines-move/data.json";
+import HandAnimationData from "../../../../../public/lottie/gpu-farm/GPU-Farm-Hand-animation/data.json";
+import PlanetAnimationData from "../../../../../public/lottie/gpu-farm/GPU-Farm-planet-animation/data.json";
 
 const FarmsAsAProduct = () => {
-  // const options = {
-  //   animationData,
-  //   loop: true,
-  //   assetsPath: "/lottie/12/images/",
-  // };
-
-  // const { View } = useLottie(options);
+  const Lottie = dynamic(() => import("lottie-react"), {
+    ssr: false,
+  });
 
   return (
     <section className="bg-[#050505] py-10 sm:py-16 md:py-20 px-4 sm:px-6 lg:px-8 flex relative flex-col items-center justify-center">
-      <img
-        src="/banner/squares_gifs/Preloder-back.gif"
-        alt="Background GIF"
-        className="absolute w-full h-full hidden md:block"
-      />
+      <motion.div
+        className="absolute w-full h-full"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          delay: 0.8,
+          duration: 2.0,
+          ease: [0.16, 1, 0.3, 1],
+        }}
+      >
+        <Lottie
+          className="w-full h-full pointer-events-none"
+          animationData={animationData}
+          loop
+        />
+      </motion.div>
 
       <div className="max-w-[1280px] mx-auto w-full relative z-10">
         <motion.div
@@ -41,39 +51,86 @@ const FarmsAsAProduct = () => {
 
         <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-5 lg:gap-8 relative">
           <motion.div
-            className="w-full rounded-lg overflow-hidden"
-            initial={{ opacity: 0, x: -20 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            viewport={{ once: true, amount: 0.3 }}
-            transition={{ duration: 0.8, ease: [0.43, 0.13, 0.23, 0.96] }}
-          >
-            <Image
-              src={"/DEFAULT/card1.webp"}
-              width={630}
-              height={450}
-              alt="GPU Farm product card 1"
-              className="w-full h-auto object-cover"
-            />
-          </motion.div>
-
-          <motion.div
-            className="w-full rounded-lg overflow-hidden"
-            initial={{ opacity: 0, x: 20 }}
-            whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true, amount: 0.3 }}
             transition={{
-              duration: 0.8,
-              ease: [0.43, 0.13, 0.23, 0.96],
-              delay: 0.2,
+              delay: 0.8,
+              duration: 2.0,
+              ease: [0.16, 1, 0.3, 1],
+            }}
+            className="relative w-full rounded-[24px] overflow-hidden"
+            style={{
+              boxShadow:
+                "inset 6px 80px 80px 0px rgba(148, 168, 237, 0.02), inset 0px -1px 1px 0px rgba(148, 168, 237, 0.20), inset 0px 1px 1px 0px rgba(148, 168, 237, 0.20)",
             }}
           >
-            <Image
-              src={"/DEFAULT/card2.webp"}
-              width={630}
-              height={450}
-              alt="GPU Farm product card 2"
-              className="w-full h-auto object-cover"
-            />
+            <motion.div className="absolute bottom-14 z-50 text-white flex flex-col gap-3 items-center">
+              <span className="font-normal text-3xl">Node Ownership</span>
+              <span className="text-center text- font-light w-[80%]">
+                Users can purchase nodes in Vyvo’s GPU farms, becoming
+                stakeholders in a rapidly growing AI infrastructure.
+              </span>
+            </motion.div>
+            <motion.div
+              className="w-full h-full"
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true, amount: 0.3 }}
+              transition={{
+                delay: 0.8,
+                duration: 2.0,
+                ease: [0.16, 1, 0.3, 1],
+              }}
+            >
+              <Lottie
+                className=" w-full h-auto object-cover pointer-events-none"
+                animationData={HandAnimationData}
+                assetsPath="/lottie/gpu-farm/GPU-Farm-Hand-animation/images/"
+                loop
+              />
+            </motion.div>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.3 }}
+            transition={{
+              delay: 0.8,
+              duration: 2.0,
+              ease: [0.16, 1, 0.3, 1],
+            }}
+            className="relative w-full rounded-[24px] overflow-hidden"
+            style={{
+              boxShadow:
+                "inset 6px 80px 80px 0px rgba(148, 168, 237, 0.02), inset 0px -1px 1px 0px rgba(148, 168, 237, 0.20), inset 0px 1px 1px 0px rgba(148, 168, 237, 0.20)",
+            }}
+          >
+            <motion.div className="absolute bottom-14 z-50 text-white flex flex-col gap-3 items-center">
+              <span className="font-normal text-3xl">Performance Benefits</span>
+              <span className="text-center text- font-light w-[80%]">
+                Distributed across strategic global locations, these farms
+                ensure high-speed, low-latency support for VAI OS users.
+              </span>
+            </motion.div>
+            <motion.div
+              className="w-full h-full"
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true, amount: 0.3 }}
+              transition={{
+                delay: 0.8,
+                duration: 2.0,
+                ease: [0.16, 1, 0.3, 1],
+              }}
+            >
+              <Lottie
+                className=" w-full h-auto object-cover pointer-events-none"
+                animationData={PlanetAnimationData}
+                assetsPath="/lottie/gpu-farm/GPU-Farm-planet-animation/images/"
+                loop
+              />
+            </motion.div>
           </motion.div>
         </div>
       </div>
