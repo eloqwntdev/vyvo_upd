cc07f063479c4a5da1964e5b5d0e3c6634e8c222
Emiro <emirodeve@gmail.com>
2025-05-08 00:44:51 +0330

    refactor: adjust HeroNews component layout for improved responsiveness

--- src/app/news/components/articles/hero.tsx
+++ src/app/news/components/articles/hero.tsx
@@ -8,7 +8,7 @@ const HeroNews = () => {
     ssr: false,
   });
   return (
-    <div className="max-w-[574px] w-full flex flex-col gap-4 md:gap-[24px] lg:gap-[32px] items-center justify-center px-[16px] md:px-[24px] lg:px-[0px]">
+    <div className="max-w-[650px] w-full flex flex-col gap-4 md:gap-[24px] lg:gap-[32px] items-center justify-center px-[16px] md:px-[24px] lg:px-[0px]">
       <div className="absolute w-full h-full left-0">
         <motion.div
           className="w-full h-full"
@@ -30,7 +30,7 @@ const HeroNews = () => {
       <h1 className="z-10 banner-gradient-text font-nb font-light text-[24px] md:text-[56px] lg:text-[72px] leading-[28px] md:leading-[60px] lg:leading-[76px] tracking-[-1.2px] md:tracking-[-1.8px] lg:tracking-[-2.2px] text-center">
         News and Updates
       </h1>
-      <p className="z-10 text-white font-nb font-light text-center text-[16px] md:text-[18px] lg:text-[20px] leading-[22px] md:leading-[24px] lg:leading-[28px] tracking-[-0.4px] md:tracking-[-0.5px] lg:tracking-[-0.6px]">
+      <p className="z-10 w-[85%] text-white font-nb font-light text-center text-[16px] md:text-[18px] lg:text-[20px] leading-[22px] md:leading-[24px] lg:leading-[28px] tracking-[-0.4px] md:tracking-[-0.5px] lg:tracking-[-0.6px]">
         Stay informed with our latest articles, press releases, and community
         updates.
       </p>
