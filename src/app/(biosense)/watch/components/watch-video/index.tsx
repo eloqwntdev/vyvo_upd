1307fa51546df24d81453e9871d580787508400e
Emiro Deve <emirodeve@gmail.com>
2025-05-15 12:19:05 +0330

    - Reduced the minimum height of the video section on smaller screens:   - Changed `min-h-[400px]` to `min-h-[220px]` to better fit mobile layouts.

--- src/app/(biosense)/watch/components/watch-video/index.tsx
+++ src/app/(biosense)/watch/components/watch-video/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const WatchVideo = () => {
   return (
-    <section className="min-h-[400px] md:min-h-[600px] lg:min-h-[900px] relative w-full">
+    <section className="min-h-[220px] md:min-h-[600px] lg:min-h-[900px] relative w-full">
       <video
         src="https://eloqwntmain.ams3.cdn.digitaloceanspaces.com/Vyvo/Werables_Page/whatch-2_SuxRJP1o.mp4"
         className="w-full h-full absolute top-0 left-0 object-cover"
