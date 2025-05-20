7a6b11f06cdb77884bcabd9abf5e1992891d0159
Emiro Deve <emirodeve@gmail.com>
2025-05-20 18:39:35 +0330

    style(preorders): center-align heading and paragraph text across all screen sizes
    - Removed conditional `sm:text-center` and applied consistent `text-center` for better layout consistency

--- src/app/(biosense)/watch/components/preorders/index.tsx
+++ src/app/(biosense)/watch/components/preorders/index.tsx
@@ -99,14 +99,14 @@ const Preorders = () => {
       >
         <motion.h2
           variants={fadeInUp}
-          className="text-white font-light font-nb text-[24px] leading-[28px] md:text-[48px] md:leading-[52px] lg:text-[56px] lg:leading-[60px] sm:text-center tracking-[-0.5px]"
+          className="text-white font-light font-nb text-[24px] leading-[28px] md:text-[48px] md:leading-[52px] lg:text-[56px] lg:leading-[60px] text-center tracking-[-0.5px]"
         >
           <span className="text-gradient">BioSense Watch</span> introduces a new
           dimension in wearable devices.
         </motion.h2>
         <motion.p
           variants={fadeInUp}
-          className="text-white font-light font-nb text-[14px] leading-[18px] md:text-[16px] md:leading-[20px] sm:text-center tracking-[-0.5px]"
+          className="text-white font-light font-nb text-[14px] leading-[18px] md:text-[16px] md:leading-[20px] text-center tracking-[-0.5px]"
         >
           Eligible buyers can now preorder BioSense Watch. Experience BioSense
           Watch for yourself, and see how this first VAI OS companion can
